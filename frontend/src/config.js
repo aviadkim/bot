b5c55e7ca6c7f1a97f91d094b30e357dd5c5925e
User <user@example.com>
2025-02-23 14:57:28 +0200

    debug8

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,7 @@
 const config = {
   apiUrl: process.env.NODE_ENV === 'production'
     ? 'https://customer-service-chatbot-production.up.railway.app'
-    : 'http://localhost:5000'
+    : 'http://localhost:5001'
 };
 
 export default config;
